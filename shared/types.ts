25c9887414eb2ac5609e0e05d004567bba4e88cc
marnix le noble <marnix.lenoble@gmail.com>
2022-04-18 13:14:58 +0200

    Add shared types

---
+++ shared/types.ts
@@ -0,0 +1,14 @@
+export interface ITask {
+  uuid: string;
+  title: string;
+  status: string;
+}
+
+export interface IUser {
+  uuid: string
+  name: string;
+}
+
+export interface IUserInput {
+  username: string;
+}
